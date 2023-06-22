11da91070e8cb843800e26cba994fa943e7e994c
zhanatkim <zhanatkim@gmail.com>
2023-06-23 03:36:32 +0600

    Добавляет стили и свайпер

--- source/js/script.js
+++ source/js/script.js
@@ -1,1 +1,16 @@
-/* в этот файл добавляет скрипты*/
+
+/* Отображение меню*/
+
+const mainNav = document.querySelector('.main-nav');
+
+const menuToggle = document.querySelector('.menu-toggle');
+
+mainNav.classList.remove('main-nav--no-js');
+menuToggle.classList.remove('menu-toggle--no-js');
+
+function openMenu () {
+  menuToggle.classList.toggle('menu-toggle--close');
+  mainNav.classList.toggle('main-nav--active');
+}
+
+menuToggle.addEventListener('click', openMenu);
