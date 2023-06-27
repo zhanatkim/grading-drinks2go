c5444327429d277889715896170c1a0647f0fe22
zhanatkim <zhanatkim@gmail.com>
2023-06-27 12:38:51 +0600

    Добавляет окончательные стили

---
+++ source/js/swiper.js
@@ -0,0 +1,12 @@
+var swiper = new Swiper(".swiper", {
+  cssMode: true,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  pagination: {
+    el: ".swiper-pagination",
+  },
+  mousewheel: true,
+  keyboard: true,
+});
